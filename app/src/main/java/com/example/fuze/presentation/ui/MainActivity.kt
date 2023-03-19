bb2e7f88032de6a6b510c3eb8232856bf071d4aa
Luiz-Adorno <luizadorno96@gmail.com>
2023-03-19 01:58:34 -0300

    Setting up the SplashScreen. - Importing the splash screen api in build.gradle. - Setting the same value themes.xml for day and night mode. - Setting the main color in colors.xml. - app_logo.xml created. - Theme.StartSplashFuze created. - Project fonts amd colors imported.

--- app/src/main/java/com/example/fuze/presentation/ui/MainActivity.kt
+++ app/src/main/java/com/example/fuze/presentation/ui/MainActivity.kt
@@ -1,10 +1,13 @@
-package com.example.fuze
+package com.example.fuze.presentation.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.example.fuze.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
